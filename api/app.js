df809110c4c81167017ebce869b3b93405265d68
Paul Hardy <paul.hardy@westfieldhealth.com>
2021-01-04 12:03:36 +0000

    Completed module

--- api/app.js
+++ api/app.js
@@ -1,14 +1,18 @@
-const { response } = require('express');
 const express = require('express');
-const { request } = require('http');
 const path = require('path');
+const router =  require('./src/router');
 
 const app = express();
 
 const pathToIndex = path.resolve(__dirname, '../client/index.html');
 
+app.use('/', router);
+
+app.use(express.static(path.resolve(__dirname, 'uploads')))
 app.use('/*', (request, response) => {
     response.sendFile(pathToIndex);
 });
+
+
 
 module.exports = app;
